d0b656e1a6bdae1d31e5088967c602b7f453de02
Kajal <kajal.singh@olx.in>
2025-04-01 23:26:55 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Kajal on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
